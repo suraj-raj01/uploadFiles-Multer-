b027c91406968b176aefb6598feff9525e3977c0
SURAJ_KUMAR <surajk38656@gmail.com>
2025-01-27 14:34:18 +0530

    Upload File (Multer)

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const PORT = 8000;
 const cors = require("cors");
 const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 const stuRoute = require("./routes/stuRoute")
 
 mongoose.connect("mongodb://127.0.0.1:27017/MulterUploadFiles").then(()=>{
@@ -15,7 +16,7 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:true}));
 
 app.use("/user",stuRoute);
-app.use('/uploads', express.static('uploads'))
+app.use('/uploads', express.static(path.join(__dirname,'uploads')));
 
 app.listen(PORT,()=>{
     console.log(`Server run on port ${PORT}`)
